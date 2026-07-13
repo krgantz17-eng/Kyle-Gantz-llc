1ceac66336b6ad1a8aedde22a9c3bd545999d95f
krgantz17-eng <krgantz17@gmail.com>
2026-07-12 23:55:53 -0400

    config: add Next.js export config for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Kyle-Gantz-llc',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
