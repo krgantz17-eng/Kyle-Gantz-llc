2fd569d4f794c69c0d7cf0170f0a370352b9ef44
krgantz17-eng <krgantz17@gmail.com>
2026-07-12 23:39:34 -0400

    feat: add Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black border-t border-gray-800 py-12">
+      <div className="max-w-6xl mx-auto px-6 text-center text-gray-400">
+        <p>&copy; 2024 Kyle Gantz LLC. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
