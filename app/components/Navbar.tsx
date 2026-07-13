260007984f63fe87047353f7acc11af085b31123
krgantz17-eng <krgantz17@gmail.com>
2026-07-12 23:39:43 -0400

    feat: add Navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-black border-b border-gray-800 sticky top-0 z-50">
+      <div className="max-w-6xl mx-auto px-6 py-4">
+        <h1 className="text-xl font-bold text-white">Kyle Gantz LLC</h1>
+      </div>
+    </nav>
+  )
+}
